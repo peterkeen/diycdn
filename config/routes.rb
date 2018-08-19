f7a21297ada9c208b06147b5fd47bf78204d6b53
Pete Keen <pete@petekeen.net>
2018-08-19 16:46:07 -0400

    Add gems and dashboards

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :proxies
+      resources :sites
+
+      root to: "proxies#index"
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/schema.rb
@@ -0,0 +1,37 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_08_19_203655) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "proxies", force: :cascade do |t|
+    t.string "external_hostname"
+    t.string "internal_hostname"
+    t.string "api_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sites", force: :cascade do |t|
+    t.string "name"
+    t.text "domain_list"
+    t.text "certificate"
+    t.text "private_key"
+    t.datetime "expires_at"
+    t.text "upstream"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
