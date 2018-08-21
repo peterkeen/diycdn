9122b604018372acb7a7ad3f3f6f61a418e7f6f1
Pete Keen <pete@petekeen.net>
2018-08-20 20:34:53 -0400

    augh

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,7 +1,7 @@
 require 'resolv'
 
 class Site < ApplicationRecord
-  before_save :refresh_certificate!
+  after_save :refresh_certificate!
 
   def refresh_certificate!
     RefreshCertificateWorker.perform_async(self.id) if domain_list_changed?
