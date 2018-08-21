958508aa7f39dc2fb91e42522eb916f5ab55c533
Pete Keen <pete@petekeen.net>
2018-08-20 21:08:45 -0400

    basic auth sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["ADMIN_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["ADMIN_PASSWORD"]))
+end if Rails.env.production?
+  
   namespace :admin do
     mount Sidekiq::Web => '/sidekiq'
 
