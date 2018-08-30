792a665fbafd576ef19f7a5bde51293ced9393f4
Pete Keen <pete@petekeen.net>
2018-08-30 08:13:16 +0200

    modules

--- lib/dns_utils.rb
+++ lib/dns_utils.rb
@@ -1,23 +1,25 @@
 require 'resolv'
 
 module DnsUtils
-  def nameservers(domain)
-    domain = domain.dup
-    result = []
+  class << self
+    def nameservers(domain)
+      domain = domain.dup
+      result = []
 
-    Resolv::DNS.open(nameserver: '8.8.8.8') do |dns|
-      while result.length == 0
-        result = dns.getresources(domain, Resolv::DNS::Resource::IN::NS).map(&:name).map(&:to_s)
-        domain = domain.split(/\./).drop(1).join('.')
+      Resolv::DNS.open(nameserver: '8.8.8.8') do |dns|
+        while result.length == 0
+          result = dns.getresources(domain, Resolv::DNS::Resource::IN::NS).map(&:name).map(&:to_s)
+          domain = domain.split(/\./).drop(1).join('.')
+        end
       end
+
+      return result
     end
 
-    return result    
-  end
-
-  def records(label, type, nameserver='8.8.8.8')
-    Resolv::DNS.open(nameserver: nameserver) do |dns|
-      dns.getresources(label, type)
+    def records(label, type, nameserver='8.8.8.8')
+      Resolv::DNS.open(nameserver: nameserver) do |dns|
+        dns.getresources(label, type)
+      end
     end
   end
 end
