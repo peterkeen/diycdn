f14d61cd4df4e29571e171b131092cfc73535bf1
Pete Keen <pete@petekeen.net>
2018-09-02 12:00:46 -0400

    bump

--- app/controllers/admin/proxies_controller.rb
+++ app/controllers/admin/proxies_controller.rb
@@ -23,7 +23,7 @@ module Admin
         proxy.update_column(:needs_setup, true)
       end
 
-      redirect_to :index, notice: "Forced setup"
+      redirect_to index_path, notice: "Forced setup"
     end
   end
 end
