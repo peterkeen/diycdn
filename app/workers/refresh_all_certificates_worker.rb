a0064f0b27d335c5de9cea407b2485516f1dc6e5
Pete Keen <pete@petekeen.net>
2018-11-04 21:53:04 -0500

    Refresh certs with 30 days to spare to quash email warnings

--- app/workers/refresh_all_certificates_worker.rb
+++ app/workers/refresh_all_certificates_worker.rb
@@ -4,7 +4,7 @@ class RefreshAllCertificatesWorker
   def perform
     Site.find_each do |site|
       certificate = OpenSSL::X509::Certificate.new site.certificate
-      next unless certificate.not_after < (Date.today + 10.days)
+      next unless certificate.not_after < (Date.today + 30.days)
 
       RefreshCertificateWorker.perform_async(site.id)
     end
